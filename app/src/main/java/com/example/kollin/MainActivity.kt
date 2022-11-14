95fb74adcfa03998b6c3d95b8c04a55e0e5a0dc3
sergeimsg <sergei-msg@tut.by>
2022-11-14 22:05:39 +0300

    Lesson 17

--- app/src/main/java/com/example/kollin/MainActivity.kt
+++ app/src/main/java/com/example/kollin/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.example.kollin
 
 import android.content.Intent
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import com.example.kollin.SecondActivity.Companion.startSecond
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.grass)))
+        supportActionBar?.title ="New title for MainActivity"
+        supportActionBar?.setIcon(R.drawable.search)
+
         setContentView(R.layout.activity_main)
         val btnGoTo = findViewById<Button>(R.id.goToSecond)
+        val btnClick = findViewById<Button>(R.id.clickMe)
+        val textView = findViewById<TextView>(R.id.valhalla)
+
+        btnClick.setOnClickListener{
+            textView.text = getString(R.string.hello)
+            startSecond(this,
+                textView.text.toString()+getString(R.string.fromMain))
+        }
         btnGoTo.setOnClickListener{
             startActivity(
                 Intent(this,SecondActivity::class.java)
@@ -18,6 +34,15 @@ class MainActivity : AppCompatActivity() {
 
 //                    .addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
             )
+            val btnClick = findViewById<Button>(R.id.clickMe)
+            val textView = findViewById<TextView>(R.id.valhalla)
+
+            btnClick.setOnClickListener{
+                val intent = Intent(this,SecondActivity::class.java)
+                startActivity(intent)
+
+                textView.text = "Hello world"
+            }
         }
     }
 }
--- app/src/main/java/com/example/kollin/SecondActivity.kt
+++ app/src/main/java/com/example/kollin/SecondActivity.kt
@@ -1,20 +1,60 @@
 package com.example.kollin
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setIcon(R.drawable.search)
+
+        val textView2 = findViewById<TextView>(R.id.textView)
+
+        val data: String = intent.getStringExtra(KEY) ?: getString(R.string.no_data)
+        textView2.text = data
+
         val btnGoTo = findViewById<Button>(R.id.goToThird)
         btnGoTo.setOnClickListener {
             startActivity(
-                Intent(this,ThirActivity::class.java)
+                Intent(this, ThirActivity::class.java)
             )
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.item_menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> onBackPressed()
+            R.id.close -> finishAffinity()
+            R.id.goBack -> onBackPressed()
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+        private const val KEY = "1"
+
+        fun startSecond(context: Context, string: String) {
+            val intent = Intent(context, SecondActivity::class.java)
+            intent.putExtra(KEY, string)
+            context.startActivity(intent)
         }
     }
 }
